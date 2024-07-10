0be7374d7e87f0b994a600574be0054e9d57f465
DJ0301 <dhananjayjoshi2003@gmail.com>
2024-07-11 00:57:07 +0530

    crawltext pauses on hold now , speed has been slowed down for smaller devices

--- client/src/components/StarWars.js
+++ client/src/components/StarWars.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './StarWars.css';
 
 const StarWars = () => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const handleTouchStart = () => {
+      textRef.current.style.animationPlayState = 'paused';
+    };
+
+    const handleTouchEnd = () => {
+      textRef.current.style.animationPlayState = 'running';
+    };
+
+    const textElement = textRef.current;
+
+    textElement.addEventListener('touchstart', handleTouchStart);
+    textElement.addEventListener('touchend', handleTouchEnd);
+
+    return () => {
+      textElement.removeEventListener('touchstart', handleTouchStart);
+      textElement.removeEventListener('touchend', handleTouchEnd);
+    };
+  }, []);
+
   return (
     <div className="star-wars-text">
-      <p className="crawl-text">
+      <p className="crawl-text" ref={textRef}>
         Episode IV<br/>
         A NEW HOPE<br/>
         Welcome to AUXO, where the Force meets blockchain. Step into a galaxy of trading opportunities where you can exchange your favorite tokens for DIAM and harness the power of decentralized finance. Whether you're a Jedi Knight or a Sith Lord of trading, AUXO empowers you to navigate the stars of the Diamante blockchain universe. May the trades be with you.
